36467fe3cc10c10a2c27450bae52fdd95671e358
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-09 10:57:03 -0500

    loadingBuilder, carga de img

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -25,7 +25,6 @@ class HerMessageBubble extends StatelessWidget {
         const SizedBox(
           height: 5,
         ),
-        //Todo Imagen
       ],
     );
   }
@@ -43,6 +42,15 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text('Mi amor esta enviando un mensaje'),
+          );
+        },
       ),
     );
   }
