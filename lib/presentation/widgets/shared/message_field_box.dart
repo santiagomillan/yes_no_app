ab3b0ac18e439e5b221197e2beaaab333027c8c1
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-09 12:31:22 -0500

    TextFormField ajustes, lectura de cambios en el imput

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final inputDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          print('valor cja de texto');
+        },
+      ),
+    );
+
+    return TextFormField(
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print(value);
+      },
+      onChanged: (value) {
+        print(value);
+      },
+    );
+  }
+}
