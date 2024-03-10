d7b93d75af5c8a548c5ba97d41913d901eacb0f0
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-09 23:02:54 -0500

    Porvider - chatScreen

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> mesages = [
+  List<Message> messageList = [
     Message(text: 'Hola mi amor!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
   ];
